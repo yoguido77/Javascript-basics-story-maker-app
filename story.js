714defc4b65f7350eb849156b9e1779c3ff37cab
yoguido77 <yoguido77@gmail.com>
2016-08-25 19:13:18 -0500

    Created story maker app

---
+++ story.js
@@ -0,0 +1,5 @@
+var nouns = prompt("Please type a noun.");
+var verbs = prompt("Please type a verb.");
+var adjectives = prompt("Please type an adjective.")
+alert("Your all done! Want to see the message?");
+document.write("You said: " + nouns + " " + verbs + " " + adjectives);
